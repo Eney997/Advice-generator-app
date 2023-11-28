7b41cd349caf1680ae442c6f2710a5d22d547339
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-11-28 16:43:57 +0400

    done xar done

---
+++ app.js
@@ -0,0 +1,20 @@
+const sharp = document.querySelector('.sharp')
+const text = document.querySelector('.text')
+const kletka = document.querySelector('.kletka')
+
+kletka.addEventListener('click', avigotData)
+
+function avigotData(){
+    fetch('https://api.adviceslip.com/advice').then(response => response.json())
+    .then(Data => {
+        const gela = Data.slip.advice;
+        const  gela2 = Data.slip.id ;
+
+        text.innerHTML = `${gela}`
+        sharp.innerHTML = `#${gela2}`
+    })
+    .catch(error =>{
+        console.log("error",error)
+        text.innerHTML = 'Oops Shechema ! Error code : 404 Shechema'
+    })
+}
